c3bcb6540e5211b5c351c271b8b92e59601579d9
Adson <seu.email@gmail.com>
2025-08-05 02:15:27 +0000

    Implementação de consumo de topicos de order e payment

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,24 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Inject } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ClientKafka, MessagePattern } from '@nestjs/microservices';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    @Inject('KAFKA_SERVICE_notification')
+    private readonly kafkaClient: ClientKafka,
+  ) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @MessagePattern('order_created')
+  handleOrderCreated(data: any): void {
+    console.log('Notificação de pedido recebido:', data);
+    // Lógica para enviar notificação
+  }
+
+  @MessagePattern('payment_succeed')
+  handlePaymentSucceed(data: any): void {
+    console.log('Notificação de pagamento recebido:', data);
+    // Lógica para enviar notificação
   }
 }
